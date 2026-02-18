394bf2f34d0dfca6979ce06d50cd88cde03c3213
shakil306 <dmshakil2026@gmail.com>
2026-02-18 11:52:27 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,33 @@
-import Image from "next/image";
-
-export default function Home() {
+function page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        voluptatum assumenda nam error molestiae dignissimos sunt modi ea
+        adipisci nulla cumque placeat quidem laboriosam inventore, fugiat
+        necessitatibus rem perferendis, incidunt vitae laborum recusandae
+        obcaecati maiores voluptatem doloremque? Dolore modi perspiciatis eius.
+        Voluptatibus doloremque reprehenderit aperiam assumenda asperiores amet
+        impedit facilis nam sit? Rem qui nostrum fugiat quam ullam hic nihil?
+        Accusantium optio dolorem distinctio asperiores aliquid sapiente nisi,
+        temporibus iusto vero repudiandae aperiam pariatur adipisci rerum
+        corporis sit. Voluptates necessitatibus est blanditiis! Voluptatum vero
+        doloribus similique. Ducimus suscipit, voluptate doloribus cupiditate
+        voluptatibus accusamus voluptatem esse omnis vitae doloremque aspernatur
+        explicabo possimus ipsa sint consectetur nostrum soluta. Dolorem tempora
+        non, eaque, sapiente praesentium neque magnam nostrum sed ut amet est
+        modi temporibus obcaecati ea quibusdam voluptatem optio hic in quod
+        dolor delectus, laudantium expedita alias. Aliquam rem quas voluptatem
+        eos minus, inventore, quae modi nostrum sed tempore veritatis
+        exercitationem perspiciatis dolores odio molestiae fuga maxime nulla
+        repellendus ducimus et eligendi unde deserunt earum pariatur! Cumque
+        voluptate enim possimus corrupti commodi placeat deserunt voluptatibus
+        qui ducimus culpa, aperiam incidunt ad voluptatem sint ex accusantium
+        suscipit omnis iure facilis! Esse debitis vitae ex explicabo obcaecati
+        recusandae sapiente. Minus molestiae sint dicta voluptates ut.
+      </h1>
     </div>
   );
 }
+
+export default page;
